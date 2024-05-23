f08ebcbb1064f14eeb21c6af72e3959c4b80e5fc
Kehindebrowns <111283128+Kehindebrowns@users.noreply.github.com>
2024-05-23 09:13:39 -0600

    Update Hello.sol
    uuupates

---
+++ Twitter's Clone/Hello.sol
@@ -0,0 +1,13 @@
+// SPDX-LISENCE-IDENTIFIER:MIT
+pragam solidity ^0.8.19;
+
+contract Hello{
+   string public name;
+
+    function SetName() public pure returns (string memory _name){
+   assert(name == "Solidty is the best programming lanugage");    
+return name = _name;
+
+}
+
+}
